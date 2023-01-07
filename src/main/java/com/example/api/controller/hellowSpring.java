ff1862cf03442a3536324fb865d3bfd534fae732
Alessandro <alessandrocosta103@gmail.com>
2023-01-07 13:59:21 -0300

    upgrade

---
+++ src/main/java/com/example/api/controller/hellowSpring.java
@@ -0,0 +1,17 @@
+package com.example.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class hellowSpring {
+
+    @GetMapping
+    public String OlaMundO(){
+        return "Hello World Spring Agita";
+
+    }
+
+}
