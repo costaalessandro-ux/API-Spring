413d2179a1d57bf44848d792b885dfff838ab0fe
Alessandro <alessandrocosta103@gmail.com>
2023-01-07 17:17:50 -0300

    cria cadastros

---
+++ src/main/java/com/example/api/controller/MedicoController.java
@@ -0,0 +1,25 @@
+package com.example.api.controller;
+
+import com.example.api.medico.DadosCadastrados;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+// o spring utiliza a notações, a anotação @RestController é para referenciar a que nossa classe faz parte de
+// um controller;
+// @RequestMapping faz com que a classe possua uma URL;
+// @PostMapping diz que a o metodo vai receber uma requisição post;
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastrados dados){
+        System.out.println(dados);
+    }
+
+
+}
