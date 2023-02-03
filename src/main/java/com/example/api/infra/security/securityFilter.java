0af8fc95c317b6cd8752a5e6eb9f051e21435a27
Alessandro <alessandrocosta103@gmail.com>
2023-02-03 18:50:28 -0300

    cria autenticação JWT

---
+++ src/main/java/com/example/api/infra/security/securityFilter.java
@@ -0,0 +1,36 @@
+package com.example.api.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class securityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    var tokenJWT = recuperarToken(request);
+
+    var subject = tokenService.getSubject(tokenJWT);
+
+    filterChain.doFilter(request, response);
+}
+
+    public String recuperarToken(HttpServletRequest request){
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null){
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+        return authorizationHeader.replace("Bearer", "");
+    }
+
+}
