0abf21f6c52cbaf3bee9fd492d6354c855b93ede
Alessandro <alessandrocosta103@gmail.com>
2023-03-12 19:00:40 -0300

    cria nova funcionalidade

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,7 +1,8 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.MedicoRepository;
-import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,11 +19,27 @@ public class AgendaDeConsultas {
     @Autowired
     private PacienteRepository pacienteRepository;
 
-    public void agendar(DadosAgendamentoConsulta dados){
+    public void agendar(DadosAgendamentoConsulta dados) throws ValidacaoException {
+        if(!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var medico = escolherMedico(dados);
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if(dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+        if(dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade é obrigat´rio quando médico não for escolhido!");
+        }
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
     }
 
 }
