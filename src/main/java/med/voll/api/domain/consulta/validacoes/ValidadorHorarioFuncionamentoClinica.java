e053e1396a766d4c856c8e2f9b75540c4d75f71c
Alessandro <alessandrocosta103@gmail.com>
2023-03-15 18:48:58 -0300

    cria interface de validação

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -2,9 +2,10 @@ package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
-
+import org.springframework.stereotype.Component;
 import java.time.DayOfWeek;
 
+@Component
 public class ValidadorHorarioFuncionamentoClinica {
 
     public void validar(DadosAgendamentoConsulta dados){
