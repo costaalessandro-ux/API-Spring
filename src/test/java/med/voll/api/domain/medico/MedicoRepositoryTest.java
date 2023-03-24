ec8e256aa020eb6a8cb09ddbaf3d267aa0144880
Alessandro <alessandrocosta103@gmail.com>
2023-03-23 23:24:30 -0300

    configura teste

---
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,19 @@
+package med.voll.api.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+//anotação para realizar o teste.
+@DataJpaTest
+// essa anotação diz para o spring não usar o banco de dados em memoria;
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioLivreNaData() {
+    }
+}
