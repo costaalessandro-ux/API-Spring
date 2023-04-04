76ec97ad6ced5fa93ea463c6a9da7819c9b70f35
Alessandro <alessandrocosta103@gmail.com>
2023-04-03 23:21:48 -0300

    cria classe medicoControllerTest

---
+++ src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -0,0 +1,24 @@
+package med.voll.api.controller;
+
+import med.voll.api.domain.medico.DadosCadastroMedico;
+import med.voll.api.domain.medico.DadosDetalhamentoMedico;
+import med.voll.api.domain.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class MedicoControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosCadastroMedico> dadosCadastroMedicoJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoMedico> dadosDetalhamentoMedicoJson;
+    
+    @MockBean
+    private MedicoRepository repository;
+
+}
