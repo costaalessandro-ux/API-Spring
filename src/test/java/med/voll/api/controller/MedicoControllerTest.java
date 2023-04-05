adaf2a827c34b448965d885820721175ab55dd6c
Alessandro <alessandrocosta103@gmail.com>
2023-04-04 23:47:43 -0300

    cria metodo para teste

--- src/test/java/med/voll/api/controller/MedicoControllerTest.java
+++ src/test/java/med/voll/api/controller/MedicoControllerTest.java
@@ -3,9 +3,12 @@ package med.voll.api.controller;
 import med.voll.api.domain.medico.DadosCadastroMedico;
 import med.voll.api.domain.medico.DadosDetalhamentoMedico;
 import med.voll.api.domain.medico.MedicoRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 public class MedicoControllerTest {
@@ -17,8 +20,14 @@ public class MedicoControllerTest {
 
     @Autowired
     private JacksonTester<DadosDetalhamentoMedico> dadosDetalhamentoMedicoJson;
-    
+
     @MockBean
     private MedicoRepository repository;
 
+    @Test
+    @DisplayName("Deveria devolver codigo http 400 quando informacoes estao invalidas")
+    @WithMockUser
+    void cadastrar_cenario1() throws Exception {
+
+    }
 }
